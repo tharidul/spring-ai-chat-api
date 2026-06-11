71c23ae9a6757af53cdf3b021ad6735b0123ac1f
Tharidu Lakmal <tharindulakmal5050@gmail.com>
2026-06-11 12:56:13 +0530

    first commit

---
+++ src/main/java/com/tharidulakmal/springaichatapi/SpringAiChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.tharidulakmal.springaichatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiChatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tharidulakmal/springaichatapi/SpringAiChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharidulakmal.springaichatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
